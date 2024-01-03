bf97675ca0fc3dd8b65493e24b38b76430487467
Sadaf <sadaf.shk88@gmail.com>
2024-01-03 20:06:39 +0530

    first program in three ways

--- Basic_programs/evenorodd.py
+++ Basic_programs/evenorodd.py
@@ -2,5 +2,11 @@ def fun1(n):
     if n%2==0:
         return True
     return False
+def fun2(n):
+    return n%2==0
+def fun3(n):
+    return (n&1)==0
 for i in range(11):
-    print(i,fun1(i),sep='\t')
+    print(i,fun1(i),fun2(i),fun3(i),sep='\t')
+
+    
